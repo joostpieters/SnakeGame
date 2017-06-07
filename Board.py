d351fdca79f225be3f49133b406f7889f05497ca
OptimisticTrousers <adam.james.clarke1@gmail.com>
2017-06-07 19:06:29 +0100

    Add files via upload

---
+++ Board.py
@@ -0,0 +1,58 @@
+Board = []
+Board_Height = 10
+Board_Length = 10
+
+## Lower ==> Higher
+##   ||
+##   ||
+##   \/
+## Higher
+
+def MakeBoard(Board, Board_Height, Board_Length): ## Do not use more than once
+    
+    for y in range(Board_Height) : ## Creates dictionary ## List of lists
+        Board.append([])           ## Will be y coord
+
+        for x in range(Board_Length) : ## x coord
+
+            if x == 0 and y == 0 :           ## checks for position and
+                Board[y].append("+")         ## sets value for board
+            elif x == Board_Length - 1 and y == 0 :
+                Board[y].append("+")
+            elif x == 0 and y == Board_Height - 1 :
+                Board[y].append("+")
+            elif x == Board_Length - 1 and y == Board_Height - 1 :
+                Board[y].append("+")
+
+            elif x == 0 :
+                Board[y].append("|")
+            elif x == Board_Length - 1 :
+                Board[y].append("|")
+
+            elif y == 0 :
+                Board[y].append("-")
+            elif y == Board_Height - 1 :
+                Board[y].append("-")
+                
+
+            else:
+                Board[y].append(" ")
+
+
+def UpdateBoard(Board, Update_y, Update_x, Update): ## Updates board with new value
+                                                    ## with Update_y and _x being the coords
+    if Update_y != 0 and Update_y != Board_Height - 1 :
+
+        if Update_x != 0 and Update_x != Board_Length - 1:
+
+            Board[Update_y][Update_x] = Update
+
+            
+        
+    
+    
+    
+            
+        
+
+    
