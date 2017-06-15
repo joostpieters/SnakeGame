375f8e675c140642f62d045fca313d14e471b829
OptimisticTrousers <adam.james.clarke1@gmail.com>
2017-06-15 10:50:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import os
-
+import time
 from Board import *
-from ReadKeyStrokes import KeyStroke
+#from ReadKeyStrokes import KeyStroke
 
-os.startfile('ReadKeyStrokes.py')
+#os.startfile('ReadKeyStrokes.py')
+Snake = ":0000"
+def SetUp(Board, PrintBoard, Snake) :
+
+    KeyStroke = "A"
+    Snake = ":0000"
+    y = 3
+    x = 1
+
+    
+    for i in Snake:
+        UpdateBoard(Board, PrintBoard, y, x, i)
+        x +=1
+
+MakeBoard(Board, Board_Height, Board_Length)
+SetUp(Board, PrintBoard, Snake)
+time.sleep(1000)
